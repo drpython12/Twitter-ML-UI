91e5ab8319a077eefbb0c24a40d04d5ebbebb8af
Palash Gandhi <gandhipalash0@gmail.com>
2021-05-24 10:54:48 +0400

    24/5/2021 10:55

--- Twitter System UI/StreamerForm.cs
+++ Twitter System UI/StreamerForm.cs
@@ -20,15 +20,24 @@ namespace Twitter_System_UI
 
         private void RunButton_Click(object sender, EventArgs e)
         {
-            string num_of_tweets = BoxNumOfTweets.Text;
-            string[] keywords = KeywordBox.Text.Split(',');
-            string args = string.Format(@"/k cd C:\Users\gandh\OneDrive\Documents\Programming\NEA\ML-Twitter-Hate-Speech-Detection & python Streamer.py {0} {1}", keywords, num_of_tweets);
-            ProcessStartInfo process = new ProcessStartInfo();
-            process.FileName = "cmd.exe";
-            process.Arguments = args;
-            Process proStart = new Process();
-            proStart.StartInfo = process;
-            proStart.Start();
+            string string_num_of_tweets = BoxNumOfTweets.Text;
+            int int_num_of_tweets; 
+            bool intResultTryParse = int.TryParse(string_num_of_tweets, out int_num_of_tweets);
+            if (intResultTryParse == false)
+            {
+                MessageBox.Show("Please input a number...");
+            }
+            else
+            {
+                string[] keywords = KeywordBox.Text.Split(',');
+                string args = string.Format(@"/k cd C:\Users\gandh\OneDrive\Documents\Programming\NEA\ML-Twitter-Hate-Speech-Detection & python Streamer.py {0} {1}", keywords, int_num_of_tweets);
+                ProcessStartInfo process = new ProcessStartInfo();
+                process.FileName = "cmd.exe";
+                process.Arguments = args;
+                Process proStart = new Process();
+                proStart.StartInfo = process;
+                proStart.Start();
+            }
         }
 
         private void CloseButton_Click(object sender, EventArgs e)
