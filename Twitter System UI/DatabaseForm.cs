ca5895d437c35e0cfd5433c84d4d2ee4a63917a0
Palash Gandhi <gandhipalash0@gmail.com>
2021-05-26 20:31:29 +0400

    26/5/2021 20:30

--- Twitter System UI/DatabaseForm.cs
+++ Twitter System UI/DatabaseForm.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
 using MySql.Data.MySqlClient;
+using System.IO;
 
 namespace Twitter_System_UI
 {
@@ -21,17 +22,39 @@ namespace Twitter_System_UI
 
         private void UploadButton_Click(object sender, EventArgs e)
         {
+            openFileDialog1.Filter = "CSV files|*.csv";
             openFileDialog1.ShowDialog();
             string uploadFilename = openFileDialog1.FileName;
-            string args = string.Format(@"/k cd C:\Users\gandh\OneDrive\Documents\Programming\NEA\ML-Twitter-Hate-Speech-Detection & python Database.py w {0}", uploadFilename);
-            ProcessStartInfo process = new ProcessStartInfo();
-            process.FileName = "cmd.exe";
-            process.Arguments = args;
-            Process proStart = new Process();
-            proStart.StartInfo = process;
-            proStart.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            proStart.Start();
-            MessageBox.Show("Success");
+            string fileEnding = Path.GetExtension(uploadFilename);
+            if (fileEnding == ".csv")
+            {
+                string args = string.Format(@"/k cd C:\Users\gandh\OneDrive\Documents\Programming\NEA\ML-Twitter-Hate-Speech-Detection & python Database.py w {0}", uploadFilename);
+                ProcessStartInfo process = new ProcessStartInfo();
+                process.FileName = "cmd.exe";
+                process.Arguments = args;
+                Process proStart = new Process();
+                proStart.StartInfo = process;
+                proStart.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                proStart.StartInfo.RedirectStandardError = true;
+                proStart.StartInfo.UseShellExecute = false;
+                try
+                {
+                    proStart.Start();
+                    StreamReader streamReader = proStart.StandardError;
+                    if (streamReader.ReadToEnd() == "")
+                    {
+                         MessageBox.Show("Success!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please choose a file that has not already been uploaded to the database!");
+                    }
+                }
+                catch (Exception c)
+                {
+                        MessageBox.Show(c.Message);
+                }
+            }
         }
 
         private void DatabaseViewButton_Click(object sender, EventArgs e)
