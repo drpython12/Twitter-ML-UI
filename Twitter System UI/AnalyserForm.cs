7d1238024c69c86efbf860fd4a98684300055162
Palash Gandhi <gandhipalash0@gmail.com>
2021-05-27 12:00:58 +0400

    27/5/2021 12:00

--- Twitter System UI/AnalyserForm.cs
+++ Twitter System UI/AnalyserForm.cs
@@ -20,20 +20,33 @@ namespace Twitter_System_UI
 
         private void UploadButton_Click(object sender, EventArgs e)
         {
+            openFileDialog1.Filter = "CSV Files|*.csv";
             openFileDialog1.ShowDialog();
         }
 
         private void RunButton_Click(object sender, EventArgs e)
         {
             string importFilename = openFileDialog1.FileName;
-            string exportFilename = ExportFileBox.Text + ".csv";
-            string args = string.Format(@"/k cd C:\Users\gandh\OneDrive\Documents\Programming\NEA\ML-Twitter-Hate-Speech-Detection & python Classifier.py {0} {1}", importFilename, exportFilename);
-            ProcessStartInfo process = new ProcessStartInfo();
-            process.FileName = "cmd.exe";
-            process.Arguments = args;
-            Process proStart = new Process();
-            proStart.StartInfo = process;
-            proStart.Start();
+            string fileEnding = System.IO.Path.GetExtension(importFilename);
+            if (fileEnding == ".csv")
+            {
+                string exportFilename = ExportFileBox.Text + ".csv";
+                string filePath = System.IO.Path.GetFullPath(exportFilename);
+                if (System.IO.File.Exists(filePath))
+                {
+                    MessageBox.Show("Filename already exists, please choose another file...");
+                }
+                else
+                {
+                    string args = string.Format(@"/k cd C:\Users\gandh\OneDrive\Documents\Programming\NEA\ML-Twitter-Hate-Speech-Detection & python Classifier.py {0} {1}", importFilename, exportFilename);
+                    ProcessStartInfo process = new ProcessStartInfo();
+                    process.FileName = "cmd.exe";
+                    process.Arguments = args;
+                    Process proStart = new Process();
+                    proStart.StartInfo = process;
+                    proStart.Start();
+                }
+            }
         }
 
         private void CloseButton_Click(object sender, EventArgs e)
